28e967ce1206f56fabba97732423b01a2d896626
Badaura <93402900+Badaura@users.noreply.github.com>
2021-12-05 08:30:24 -0800

    Update special list.lua

--- special list.lua
+++ special list.lua
@@ -12,5 +12,5 @@ return owner
 return mods
 --[[
 if u want to be vip rank u need to be friend i mean really friend of one of the creators (in some cases mods) and then ask the owners if u can get that rank
-the list of vip will incrisi
+the list of vip will incres, thx!
 ]]
