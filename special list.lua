6a9addec6c566ff7650746a420bb93dba2c19bce
Badaura <93402900+Badaura@users.noreply.github.com>
2021-12-07 16:46:43 -0800

    Update special list.lua

--- special list.lua
+++ special list.lua
@@ -1,10 +1,9 @@
---
 local mods = {
     "01xBams",
 }
-return mods
 --[[
 if u want to be vip rank u need to be friend i mean really friend of one of the creators (in some cases mods) and then ask the owners if u can get that rank
 the list of vip will incres, thx!
 ]]
---
+
+return mods
