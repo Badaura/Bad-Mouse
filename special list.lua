cb61b79eef96c686204a2e631cc45569fd97e3df
Badaura <93402900+Badaura@users.noreply.github.com>
2021-12-13 09:17:32 -0800

    Delete special list.lua

--- special list.lua
+++
@@ -1,9 +0,0 @@
-local mods = {
-    "01xBams",
-}
---[[
-if u want to be vip rank u need to be friend i mean really friend of one of the creators (in some cases mods) and then ask the owners if u can get that rank
-the list of vip will incres, thx!
-]]
-
-return mods
